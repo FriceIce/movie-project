a68e1246d1c67970f0c7bfa693f08adffd441729
Isak Tilahun <tilahun.isak@gmail.com>
2025-04-05 16:26:15 +0200

    I have added a Prettier config for consistent code styling.

--- server/src/utils/error/errorFunc.ts
+++ server/src/utils/error/errorFunc.ts
@@ -1,25 +1,24 @@
-import { consoleLog } from "../logger";
-import { CustomError } from "./error";
-import { Response } from "express";
+import { consoleLog } from '../logger';
+import { CustomError } from './error';
+import { Response } from 'express';
 // import {}
 
 export function errorHandler(error: unknown, res: Response) {
-  if(error instanceof Error) {
-    consoleLog('error', 'Error: ' + error.message); 
+    if (error instanceof Error) {
+        consoleLog('error', 'Error: ' + error.message);
 
-    if(error instanceof CustomError.EmailError) {
-        return res.status(error.statusCode).json({ 
-          message: error.message, 
-        }); 
-      }
-    
-    if(error instanceof CustomError.PasswordError) {
-      return res.status(error.statusCode).json({ 
-        message: error.message, 
-      })
+        if (error instanceof CustomError.EmailError) {
+            return res.status(error.statusCode).json({
+                message: error.message,
+            });
+        }
+
+        if (error instanceof CustomError.PasswordError) {
+            return res.status(error.statusCode).json({
+                message: error.message,
+            });
+        }
+
+        return res.status(500).json({ message: 'Something went wrong' });
     }
-  
-    return res.status(500).json({ message: 'Something went wrong' });
-  }
-
-} 
+}
