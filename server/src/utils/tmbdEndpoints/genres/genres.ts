8bdeafc31c70bfb20016c453786666c1c66adb05
Isak Tilahun <tilahun.isak@gmail.com>
2025-04-05 21:06:11 +0200

    Added JSDoc to functions.

--- server/src/utils/tmbdEndpoints/genres/genres.ts
+++ server/src/utils/tmbdEndpoints/genres/genres.ts
@@ -1,12 +1,14 @@
 import { fetchConfig, fetchResponse } from '../../helperFuncs';
 import { genresUrl } from '../assets/genres';
 
-export async function genres(type: 'movie' | 'tv'): Promise<Genres | null> {
+/**
+ * Retrieves a list of genres for movies or TV shows
+ * @param type
+ */
+export default async function genres(type: 'movie' | 'tv'): Promise<Genres | null> {
     const { options } = fetchConfig('GET');
     const url = type === 'movie' ? genresUrl.movie : genresUrl.tv;
 
     const response = await fetchResponse<Genres>('get', url, options);
-    // console.table(response?.genres)
-
     return response;
 }
--- server/src/utils/tmbdEndpoints/movie/movieDetails.ts
+++ server/src/utils/tmbdEndpoints/movie/movieDetails.ts
@@ -2,7 +2,11 @@ import { MovieDetail } from '../../../types/movies';
 import { fetchConfig, fetchResponse } from '../../helperFuncs';
 import { detailsUrl } from '../assets/details';
 
-export async function movieDetails(id: string): Promise<MovieDetail.Response | null> {
+/**
+ * Retrieves an object containg movie details.
+ * @param {string} id
+ */
+export default async function movieDetails(id: string): Promise<MovieDetail.Response | null> {
     const { options } = fetchConfig('GET', [id]);
     const url = detailsUrl(id).movie;
 
