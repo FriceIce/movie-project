742075686fd352c908fc718cdca3c2ac79f9e5c5
Isak Tilahun <tilahun.isak@gmail.com>
2025-04-08 19:17:20 +0200

    Removed unused code.

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,16 +5,6 @@ import cors from 'cors';
 import { consoleLog } from './utils/logger';
 import { movieRoutes, userRoutes } from './routes/index';
 import 'dotenv/config';
-import {
-    genres,
-    details,
-    discovery,
-    recommendations,
-    topRated,
-    popular,
-    trending,
-    search,
-} from './utils/api/tmbd';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,18 +15,10 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan('combined'));
 
-// discovery('movie', '1', ['with_genres=28']);
-// genres('tv');
-// details('movie', '950387');
-// recommendations('movie', '1396', '1');
-// topRated('movie', '1');
-// popular('tv', '1');
-// trending('movie');
-// search('movie', 'man on fire');
-
 // routes
 app.use('/api', userRoutes);
 app.use('/api', movieRoutes);
+
 app.listen(port, () => {
     consoleLog('highlight', `Server is running on port ${port}`);
 });
