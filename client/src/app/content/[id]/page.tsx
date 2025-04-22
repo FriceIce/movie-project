69c8ec4199fc3747ba6a5c6e23c4fd8b246a1d52
Isak Tilahun <tilahun.isak@gmail.com>
2025-04-22 21:11:53 +0200

    Added folders to the src directory inside the client directory.

---
+++ client/src/app/content/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+type Props = {
+    params: {
+        id: string;
+    };
+};
+const Content = ({ params }: Props) => {
+    return <div>Content/{params.id} Page</div>;
+};
+
+export default Content;
---
+++ client/src/app/tv/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+    return <div>TV Page</div>;
+};
+
+export default page;
