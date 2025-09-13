1037841f8767214d1c72b27713f4c1f7c9393f1e
Isak Tilahun <tilahun.isak@gmail.com>
2025-09-13 11:55:54 +0200

    fix: prevent crash when visiting page without tokens.

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -19,5 +19,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/', '/movies', '/home', '/content/:path*', '/tv'],
+    matcher: ['/', '/movies', '/home', '/content/:path*', '/tv', '/genre/:path*'],
 };
